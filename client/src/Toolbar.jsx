929f3c965e18170ae249779c07824be2ad2b7c8a
francis-asdf <114354354+francis-asdf@users.noreply.github.com>
2026-07-05 17:07:49 -0700

    replace emojis with standardized icons in toolbar

--- client/src/Toolbar.jsx
+++ client/src/Toolbar.jsx
@@ -1,31 +1,37 @@
 import './toolbar.css';
+import PencilIcon from '@iconify-react/mdi/pencil';
+import MouseIcon from '@iconify-react/material-symbols/mouse';
+import TextIcon from '@iconify-react/mdi/text';
+import LineSegmentIcon from '@iconify-react/ph/line-segment';
+import TrashIcon from '@iconify-react/mdi/trash';
+import SaveIcon from '@iconify-react/material-symbols/save';
 
 export default function Toolbar({ tool, setTool, handleClear, handleSave }) {
     return (
         <div className="toolbar">
             <button className={tool === "draw" ? "active" : ""}
                 onClick={() => setTool("draw")}>
-                ✏️
+                <PencilIcon height="1.5em" />
             </button>
             <button className={tool === "select" ? "active" : ""}
                 onClick={() => setTool("select")}>
-                🖱️
+                <MouseIcon height="1.5em" />
             </button>
             <button className={tool === "text" ? "active" : ""}
                 onClick={() => setTool("text")}>
-                T
+                <TextIcon height="1.5em" />
             </button>
             <button className={tool === "line" ? "active" : ""}
                 onClick={() => setTool("line")}>
-                L
+                <LineSegmentIcon height="1.5em" />
             </button>
             <button className="clear"
                 onClick={handleClear}>
-                🗑️
+                <TrashIcon height="1.5em" />
             </button>
             <button className="save"
                 onClick={() => handleSave(false, true)}>
-                💾
+                <SaveIcon height="1.5em" />
             </button>
         </div>
     );
